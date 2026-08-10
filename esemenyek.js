33d303fd035f7e8a70622596ffb093b50f1b78a0
iG6500 <gorotyaki@gmail.com>
2026-08-10 15:42:29 +0200

    Update seating availability for event

--- esemenyek.js
+++ esemenyek.js
@@ -61,8 +61,7 @@ window.BACSKAI_ESEMENYEK = [
     ido: "17:30–19:30",
     helyszin: "Baja, képzőterem",
     ferohely: 25,
-    foglalt: 0,
-    telt: true,
+    foglalt: 22,
   },
   {
     kepzes: "szerdai",
